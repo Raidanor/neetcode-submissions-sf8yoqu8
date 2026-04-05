fbcd8de642e8cfdf862610a160c8083609c77892
Raidanor <76647556+Raidanor@users.noreply.github.com>
2026-04-05 16:28:22 -0300

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let count = []
+        let l = 0
+        let max = 0
+        let result = 0
+
+        for (let r=0; r<s.length; ++r){
+            if (count[s[r]] == null)
+                count[s[r]] = 1
+            else
+                count[s[r]] += 1
+
+            max = Math.max(max, count[s[r]])
+
+            if ((r-l+1) - max <= k){
+                result = r - l + 1
+            } else {
+                count[s[l]] -= 1
+                l += 1
+            }
+        }
+
+        console.log(result)
+        console.log(count)
+        console.log(max)
+        return result
+
+        
+    }
+}
