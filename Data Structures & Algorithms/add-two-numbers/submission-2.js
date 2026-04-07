28e939c038f747a8a8520debe344c930a5aeae13
Raidanor <76647556+Raidanor@users.noreply.github.com>
2026-04-07 04:02:53 -0300

    Add: add-two-numbers - submission-2

---
+++ Data Structures & Algorithms/add-two-numbers/submission-2.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} l1
+     * @param {ListNode} l2
+     * @return {ListNode}
+     */
+    addTwoNumbers(l1, l2) {
+        let result = new ListNode()
+        let cur = result
+        let carry = 0
+        
+        while (l1 || l2 || carry ){
+            let val1 = l1 ? l1.val : 0
+            let val2 = l2 ? l2.val : 0
+
+            let sum = val1 + val2 + carry
+
+           
+            if (sum > 9){
+                sum = sum % 10
+                carry = 1
+            } else {
+                carry = 0
+            }
+
+            cur.next = new ListNode(sum)
+            cur = cur.next
+
+            l1 = l1 ? l1.next : null
+            l2 = l2 ? l2.next : null
+        }
+            
+        return result.next
+
+    }
+}
