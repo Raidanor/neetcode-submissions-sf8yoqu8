941fdda3e5c4eea157a925810d3b0e0f58d7937a
Raidanor <76647556+Raidanor@users.noreply.github.com>
2026-04-13 19:15:07 -0300

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let freq = []
+        let res = {}
+        for (let i=0; i< strs.length; ++i){
+            const sorted = strs[i].split('').sort().join('')
+
+            if (!res[sorted]){
+                res[sorted] = []
+            }
+            res[sorted].push(strs[i])
+
+        }
+        return Object.values(res)
+    }
+}
