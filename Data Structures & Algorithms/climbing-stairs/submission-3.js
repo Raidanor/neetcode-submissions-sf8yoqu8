c7059b5cb77ccc0f73900d42d3a9422b81143e7d
Raidanor <76647556+Raidanor@users.noreply.github.com>
2026-04-13 19:51:43 -0300

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n) {
+        if (n<2)
+            return 1
+        else if (n<3)
+            return 2
+        
+        let cache = []
+        let total = 0
+        cache[n-1] = 1
+        cache[n-2] = 1
+
+        for (let i=n-3; i>=0; --i){
+            console.log(i)
+            cache[i] = cache[i+1] + cache[i+2] 
+        }
+
+        total = cache[0] + cache[1]
+        return total
+    }
+}
